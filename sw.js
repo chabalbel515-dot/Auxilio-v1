637be054e89eedcada4e4ad202d4ded0dc0d3d41
Z.sXis <chabalbel515@gmail.com>
2026-02-28 12:24:55 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'zs-xis-cache-v1';
+const assets = [
+  './index.html',
+  './painel_script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
