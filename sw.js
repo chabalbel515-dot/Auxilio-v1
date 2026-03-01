d7f0908b1cea7ede742fce5ecd33094097e55761
Z.sXis <chabalbel515@gmail.com>
2026-02-28 21:40:51 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'sielz-engine-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './engine.js',
+    './manifest.json',
+    './1000053165.jpg'
+];
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            const fetchUpdate = fetch(e.request).then(netRes => {
+                caches.open(CACHE_NAME).then(c => c.put(e.request, netRes.clone()));
+                return netRes;
+            });
+            return res || fetchUpdate;
+        })
+    );
+});
